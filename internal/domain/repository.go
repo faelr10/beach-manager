f60360d75acc444cd7f90478f76ebd00fdfa4807
Rafael <rafaelv.boscato@hotmail.com>
2025-04-12 11:40:45 -0300

    add AgendaRepository interface

--- internal/domain/repository.go
+++ internal/domain/repository.go
@@ -5,3 +5,9 @@ type UserRepository interface {
 	GetByID(id string) (*User, error)
 	GetByEmail(email string) (*User, error)
 }
+
+type AgendaRepository interface {
+	Create(agenda *Agenda) error
+	GetByID(id string) (*Agenda, error)
+	GetAll() ([]*Agenda, error)
+}
